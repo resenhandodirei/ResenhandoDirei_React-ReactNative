61ed6a1f0b79bb38a920bcf73a33961ea2932a93
resenhandodirei <larimscorrea@gmail.com>
2025-01-12 00:16:25 -0300

    feat: aprimorando o componente de imagem

--- resenhandodirei/src/components/Images/Image.tsx
+++ resenhandodirei/src/components/Images/Image.tsx
@@ -1,11 +1,35 @@
-interface Image {
-    id: number
+import React from 'react';
+
+// Importação das imagens
+import img1 from '../../../assets/1.jpg';
+import img2 from '../../../assets/2.jpg';
+import img3 from '../../../assets/3.jpg';
+import img4 from '../../../assets/4.jpg';
+import img5 from '../../../assets/5.jpg'; 
+
+interface ImageProps {
+    id: number;
 }
 
-export const Image = ({ id }: Image) => {
+const imageMap = {
+    1: img1,
+    2: img2,
+    3: img3,
+    4: img4,
+    5: img5
+};
+
+export const Image = ({ id }: ImageProps) => {
+    // Obtém a imagem correspondente ao id
+    const imageSrc = imageMap[id];
+
+    if (!imageSrc) {
+        return <div>Imagem não encontrada</div>;  // Caso a imagem não exista
+    }
+
     return (
         <div>
-            <img src={`../../../assets/${id}.jpg`} alt={`Feedback ${id}`} />
+            <img src={imageSrc} alt={`Feedback ${id}`} />
         </div>
-    )
-}
+    );
+};
